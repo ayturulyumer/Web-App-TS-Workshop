5b88d37dc7464bdad8bd70db9a4fe6bad5860b1f
ajummer <makeudreamempty@gmail.com>
2024-04-24 10:40:47 +0300

    init & add types

---
+++ src/Web-App-TS-Workshop/src/type/user.ts
@@ -0,0 +1,29 @@
+export type Geo = {
+  lat: string;
+  lng: string;
+};
+
+export type Adress = {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+};
+
+export type Company = {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+};
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Adress;
+  phone: string;
+  website: string;
+  company: Company;
+};
